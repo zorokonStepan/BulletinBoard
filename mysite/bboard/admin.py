77d56f158a6a867e628c707b5c67dc8f78e73a67
Stepan Kolodkin <zorokon@yandex.ru>
2023-10-08 15:22:35 +0300

    create class BboardAdmin

--- mysite/bboard/admin.py
+++ mysite/bboard/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-
 from .models import Bboard
 
 
-admin.site.register(Bboard)
+class BboardAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content', )
+
+
+admin.site.register(Bboard, BboardAdmin)
