af30ef2076ab79aa6d19959ee4bdaf9b593557b9
Stepan Kolodkin <zorokon@yandex.ru>
2023-10-08 14:32:10 +0300

    create template bboard.index

--- mysite/bboard/views.py
+++ mysite/bboard/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
+from .models import Bboard
+
 
 def index(request: HttpRequest):
-    return HttpResponse("список объявлений.")
+    title = 'Список объявлений\r\n\r\n\r\n'
+
+    data = [title]
+    for bb in Bboard.objects.order_by('-published'):
+        data.append(f'{bb.title}\r\n{bb.content}\r\n\r\n')
+    return HttpResponse(''.join(data), content_type='text/plain; charset=utf-8')
+
