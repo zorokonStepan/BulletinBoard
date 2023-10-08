f29db8da10b30ed3674f0761fa82eac78b4d690e
Stepan Kolodkin <zorokon@yandex.ru>
2023-10-08 13:47:12 +0300

    create bboard.urls

--- mysite/bboard/views.py
+++ mysite/bboard/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+
+def index(request: HttpRequest):
+    return HttpResponse("список объявлений.")
